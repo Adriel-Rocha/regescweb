8e257799895360cb9f222880a58db53865b29f8c
lindbergh <lindbergh@kali>
2024-11-08 17:50:43 -0300

    create entity Professor

---
+++ src/main/java/br/com/adrielrocha/regescweb/models/Professor.java
@@ -0,0 +1,32 @@
+package br.com.adrielrocha.regescweb.models;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Professor {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    private BigDecimal salario;
+    @Enumerated(EnumType.STRING)
+    private StatusProfessor statusProfessor;
+
+
+}
