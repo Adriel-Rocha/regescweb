cb81c896b6e956ccfbb570fc5dc057446cefbbed
Adriel Francisco Rocha <adrielfr194@gmail.com>
2024-11-12 19:58:08 -0300

    improvement: error handling

--- src/main/java/br/com/adrielrocha/regescweb/controllers/ProfessorController.java
+++ src/main/java/br/com/adrielrocha/regescweb/controllers/ProfessorController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +13,7 @@ import br.com.adrielrocha.regescweb.dto.RequisicaoNovoProfessor;
 import br.com.adrielrocha.regescweb.models.Professor;
 import br.com.adrielrocha.regescweb.models.StatusProfessor;
 import br.com.adrielrocha.regescweb.repositories.ProfessorRepository;
+import jakarta.validation.Valid;
 
 @Controller
 public class ProfessorController {
@@ -32,21 +34,31 @@ public class ProfessorController {
         return mv;
     }
 
-    @GetMapping("/professor/new")
-    public ModelAndView nnew() {
+    @GetMapping("/professores/new")
+    public ModelAndView nnew(RequisicaoNovoProfessor req) {
 
         ModelAndView mv = new ModelAndView("professores/new");
-        mv.addObject("statusProfessor", StatusProfessor.values());
+        mv.addObject("listaStatusProfessor", StatusProfessor.values());
 
         return mv;
     }
 
     @PostMapping("/professores")
-    public String create(RequisicaoNovoProfessor req) {
-        Professor professor = req.toProfessor();
-        this.professorRepository.save(professor);
+    public ModelAndView create(@Valid RequisicaoNovoProfessor req, BindingResult bindingResult) {
 
-        return "redirect:/professores";
+        if (bindingResult.hasErrors()) {
+            System.out.println("********************* ---------- ******************");
+
+            ModelAndView mv = new ModelAndView("professores/new");
+            mv.addObject("listaStatusProfessor", StatusProfessor.values());
+            return mv;
+            
+        } else {
+            Professor professor = req.toProfessor();
+            this.professorRepository.save(professor);
+        
+            return new ModelAndView("redirect:/professores");
     }
+}
 
 }
--- src/main/java/br/com/adrielrocha/regescweb/dto/RequisicaoNovoProfessor.java
+++ src/main/java/br/com/adrielrocha/regescweb/dto/RequisicaoNovoProfessor.java
@@ -4,13 +4,20 @@ import java.math.BigDecimal;
 
 import br.com.adrielrocha.regescweb.models.Professor;
 import br.com.adrielrocha.regescweb.models.StatusProfessor;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RequisicaoNovoProfessor {
+    @NotBlank
+    @NotNull
     private String nome;
+    @NotNull
+    @DecimalMin(value = "0.0")
     private BigDecimal salario;
     private StatusProfessor statusProfessor;
 
